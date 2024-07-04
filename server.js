51d8a0bff9ee2a525418df20fe267f2be8eec52a
LevazeDigital <147309264+LevazeDigital@users.noreply.github.com>
2024-07-04 16:50:37 +0530

    my commit

--- server.js
+++ server.js
@@ -4,12 +4,30 @@ let ejs = require('ejs')
 let expressLayout = require('express-ejs-layouts');
 let app = express()
 const PORT = process.env.PORT || 9000;
-app.get("/", (req, resp) => {
-    resp.render('home');
-})
+//assets
+app.use(express.static('public'))
+
+
 app.use(expressLayout);
 app.set('views', path.join(__dirname, "resources/views"))
 app.set('view engine', 'ejs');
+
+app.get("/", (req, resp) => {
+    resp.render('home');
+})
+
+app.get("/cart", (req, resp) => {
+    resp.render("customers/cart");
+})
+
+app.get("/login", (req, resp) => {
+    resp.render("auth/login");
+})
+
+app.get("/register", (req, resp) => {
+    resp.render("auth/register");
+})
+
 app.listen(PORT, () => {
     console.log('server is running on Port 9000')
 })
